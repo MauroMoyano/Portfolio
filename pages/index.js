5b8534f0c3c3277df71a298d3fe33d2414c567c5
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-04-09 22:39:21 -0300

    preparando el codigo para trabajar con variables en css

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,13 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const text = "Mauro Crea su portfolio para poder mostrar sus habilidades en ciertas tecnologias"
+  const caracteres = text.length // type number, this for CSS
+
   return (
     <>
     <span>
-    <h1>Mauro Crea su portfolio</h1>
+    <h1>{text}</h1>
     </span>
     </>
   )
