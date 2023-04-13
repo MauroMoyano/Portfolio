5c38832bb6529225475139bed71759b79fd805df
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-04-12 23:42:43 -0300

    style nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Nav () {
     return(
         <>
-        <div ClassName="">
+        <div ClassName="nav">
             <Link href="#about"> About </Link>
             <Link href="#projects"> Projects </Link>
             <Link href="#contact"> Contact </Link>
