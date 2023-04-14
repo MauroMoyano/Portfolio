8a169b68b718ba83d364ab7f89dd810fed8387fe
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-04-13 23:14:45 -0300

    Created Style nav2

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -5,9 +5,9 @@ export default function Nav () {
     return(
         <>
         <div className={style.nav}>
-            <Link href="@/components/Nav/Nav#about"> About </Link>
-            <Link href="@/components/Nav/Nav#projects"> Projects </Link>
-            <Link href="@/components/Nav/Nav#contact"> Contact </Link>
+            <Link href="@/components/Nav/Nav#about" className={style.nav}> About </Link>
+            <Link href="@/components/Nav/Nav#projects" className={style.nav}> Projects </Link>
+            <Link href="@/components/Nav/Nav#contact" className={style.nav}> Contact </Link>
         </div>
         </>
     )
