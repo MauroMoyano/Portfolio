8c18a2ba5d45fc105f68952e966d6f3989a31c33
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-05-05 01:21:49 -0300

    Trabajando en ccs de proyectos haciendo la grilla

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,14 +1,38 @@
-export default function Contact () {
-    return(
-        <>
-        <h1>Contact me</h1>
-        <form >
-            <input type="text" placeholder="Tu nombre" className="appearance-none border rounded w-40% py-2 px-3 bg-slate-800 text-green-400 leading-tight focus:outline-none focus:shadow-outline" id="name" />
-            <input type="email" placeholder="Tu correo electronico" className="appearance-none border rounded w-40% py-2 px-3 bg-slate-800 text-green-400 leading-tight focus:outline-none focus:shadow-outline" id="email" />
-            <textarea id="userMenssage" cols="30" rows='10' placeholder="Deja tu mensaje" className="appearance-none border rounded w-60% py-2 px-3 block mb-2 bg-slate-800 text-green-400 " />
-        </form>
-
-        </>
-
-    )
+export default function Contact() {
+  return (
+    <>
+      <h1>Contact me</h1>
+      <form>
+        <div className="flex justify-end">
+            <div className="col-2">
+          <div className="w-200 h-12 mr-4">
+            <input
+              type="text"
+              placeholder="Tu nombre"
+              className="appearance-none border rounded py-2 px-3 bg-slate-800 text-green-400 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+            />
+          </div>
+          <div className="w-200 h-24 mr-4">
+            <input
+              type="email"
+              placeholder="Tu correo electronico"
+              className="appearance-none border rounded w-40% py-2 px-3 bg-slate-800 text-green-400 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+            />
+          </div>
+          </div>
+          <div className="w-200 h-200 mr-6">
+            <textarea
+              id="userMenssage"
+              cols="30"
+              rows="10"
+              placeholder="Deja tu mensaje"
+              className="appearance-none border rounded w-60% py-2 px-3 mb-2 bg-slate-800 text-green-400 align-end"
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,32 +1,71 @@
-import Image from "next/image"
-import Link from "next/link"
-export default function Projects(){
-    return (
-      <>
-      
-       <div className="border border-blue-200 opacity-50 text-center space-x-60 py-6">
-         <Link href="#" className="btn btn-blue btn-blue:hover">Todos</Link>
-         <Link href="#" className="btn btn-blue btn-blue:hover">Academicos</Link>
-         <Link href="#" className="btn btn-blue btn-blue:hover">FreeLancer</Link>
-       <div className="flex flex-wrap py-4 space-x-6">
-       <a href="#">
-        <Image src="/img/emprendar.jpg" alt="Emprendar" width={250} height={150}/>
-       </a>
-       <a href="#">
-        <Image src="/img/emprendar.jpg" alt="Emprendar" width={250} height={150}/>
-       </a>
-       <a href="#">
-        <Image src="/img/emprendar.jpg" alt="Emprendar" width={250} height={150}/>
-       </a>
-       <a href="#">
-        <Image src="/img/emprendar.jpg" alt="Emprendar" width={250} height={150}/>
-       </a>
-       <a href="#">
-        <Image src="/img/emprendar.jpg" alt="Emprendar" width={250} height={150}/>
-       </a>
-       </div>
-       </div>
-
-      </>
-    )
+import Image from "next/image";
+import Link from "next/link";
+export default function Projects() {
+  return (
+    <>
+      <div className="border border-blue-200 opacity-50 text-center space-x-60 py-6">
+        <Link href="#" className="btn btn-blue btn-blue:hover">
+          Todos
+        </Link>
+        <Link href="#" className="btn btn-blue btn-blue:hover">
+          Academicos
+        </Link>
+        <Link href="#" className="btn btn-blue btn-blue:hover">
+          FreeLancer
+        </Link>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="w-24 h-24 place-items-center">
+            <a href="#">
+              <Image
+                src="/img/emprendar.jpg"
+                alt="Emprendar"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+          <div className="w-24 h-24 place-items-center">
+            <a href="#">
+              <Image
+                src="/img/emprendar.jpg"
+                alt="Emprendar"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+          <div className="w-24 h-24 place-items-center">
+            <a href="#">
+              <Image
+                src="/img/emprendar.jpg"
+                alt="Emprendar"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+          <div className="w-24 h-24 place-items-center">
+            <a href="#">
+              <Image
+                src="/img/emprendar.jpg"
+                alt="Emprendar"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+          <div className="w-24 h-24 place-items-center">
+            <a href="#">
+              <Image
+                src="/img/emprendar.jpg"
+                alt="Emprendar"
+                width={250}
+                height={150}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
