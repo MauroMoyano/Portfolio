1346b2567125e6d8584bf19452816f9c7c4f4a22
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-05-08 23:44:20 -0300

    bg for components

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,44 @@
-import About from "@/components/About/About"
-import Contact from "@/components/Contact/Contact"
-import Footer from "@/components/Footer/Footer"
-import Nav from "../components/Nav/Nav"
-import Projects from '../components/Projects/projects'
-import { Inter } from 'next/font/google'
-import Skills from "@/components/Skills/Skills"
+import About from "@/components/About/About";
+import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
+import Nav from "../components/Nav/Nav";
+import Projects from "../components/Projects/projects";
+import { Inter } from "next/font/google";
+import Skills from "@/components/Skills/Skills";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const text = "Mauro Crea su portfolio para poder mostrar sus habilidades en ciertas tecnologias."
-  const caracteres = text.length // type number, this for CSS.
+  const text =
+    "Mauro Crea su portfolio para poder mostrar sus habilidades en ciertas tecnologias.";
+  const caracteres = text.length; // type number, this for CSS.
 
   return (
-    <> 
+    <>
       <title>MauroMoyanoDev</title>
       <header calss="">
         <div>
           <h1>Mauro Moyano</h1>
           <p>Full Stack Web Developer</p>
         </div>
-      <Nav />
-</header>
-    <div>
-      <Projects />
-    <h1>{text}</h1>
-    </div>
-    <div>
-    </div>
-    
-    <Skills />
-    <About />
-    <Contact />
-    <Footer />
+        <Nav />
+      </header>
+      <div>
+        <Projects />
+        <h1>{text}</h1>
+      </div>
+      <div>
+        <Skills />
+      </div>
+      <div className="bg-auto bg-slate-500">
+        <About />
+      </div>
+      <div className="bg-auto bg-zinc-900">
+      <Contact />
+      </div>
+      <div className="bg-auto bg-slate-500">
+      <Footer />
+      </div>
     </>
-  )
+  );
 }
